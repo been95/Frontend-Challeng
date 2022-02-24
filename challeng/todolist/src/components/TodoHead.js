879316de2ff60a8b6491fb1b651410ee9e64915a
bee95 <dcb95@naver.com>
2022-02-24 18:29:53 +0900

    할일입력

--- challeng/todolist/src/components/TodoHead.js
+++ challeng/todolist/src/components/TodoHead.js
@@ -1,50 +1,45 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useTodoState } from '../TodoContext';
 
 const TodoHeadBlock = styled.div`
-  padding-top: 48px;
-  padding-left: 32px;
-  padding-right: 32px;
-  padding-bottom: 24px;
-  border-bottom: 1px solid #e9ecef;
-  h1 {
-    margin: 0;
-    font-size: 36px;
-    color: #343a40;
-  }
-  .day {
-    margin-top: 4px;
-    color: #868e96;
-    font-size: 21px;
-  }
-  .tasks-left {
-    color: #20c997;
-    font-size: 18px;
-    margin-top: 40px;
-    font-weight: bold;
-  }
+    padding-top: 48px;
+    padding-left: 32px;
+    padding-right: 32px;
+    padding-bottom: 24px;
+    border-bottom: 1px solid #e9ecef;
+    .text_wrap li{
+        display:inline-block;
+    }
+    h1 {
+        margin: 0;
+        font-size: 36px;
+        color: #9c67a9;
+    }
+    .day {
+        color: #868e96;
+        font-size: 18px;
+        margin-left: 20px;
+        
+    }
+    .tasks {
+        color: #474747;
+        font-size: 18px;
+        margin-top: 20px;
+        font-weight: bold;
+        text-align:center;
+    }
 `;
 
 function TodoHead() {
-  const todos = useTodoState();
-  const undoneTasks = todos.filter(todo => !todo.done);
-
-  const today = new Date();
-  const dateString = today.toLocaleDateString('ko-KR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-  });
-  const dayName = today.toLocaleDateString('ko-KR', { weekday: 'long' });
-
-  return (
-    <TodoHeadBlock>
-      <h1>{dateString}</h1>
-      <div className="day">{dayName}</div>
-      <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
-    </TodoHeadBlock>
-  );
+    return (
+        <TodoHeadBlock>
+        <ul className="text_wrap">
+            <li><h1>2022년 2월 22일</h1></li>
+            <li className="day">화요일</li>
+        </ul>
+        <div className="tasks">할 일 2개 남음</div>
+        </TodoHeadBlock>
+    );
 }
 
 export default TodoHead;
--- challeng/todolist/src/components/TodoList.js
+++ challeng/todolist/src/components/TodoList.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import TodoItem from './TodoItem';
-import { useTodoState } from '../TodoContext';
 
 const TodoListBlock = styled.div`
-  flex: 1;
-  padding: 20px 32px;
-  padding-bottom: 48px;
-  overflow-y: auto;
+    flex: 1;
+    padding: 20px 32px;
+    padding-bottom: 48px;
+    overflow-y: auto;
 `;
 
 function TodoList() {
-  const todos = useTodoState();
-
-  return (
-    <TodoListBlock>
-      {todos.map(todo => (
-        <TodoItem
-          key={todo.id}
-          id={todo.id}
-          text={todo.text}
-          done={todo.done}
-        />
-      ))}
-    </TodoListBlock>
-  );
+    return (
+        <TodoListBlock>
+        <TodoItem text="일본어일기" done={true} />
+        <TodoItem text="영어독해숙제" done={true} />
+        <TodoItem text="TODOLIST 만들기" done={false} />
+        <TodoItem text="일러스트숙제" done={false} />
+        </TodoListBlock>
+    );
 }
 
 export default TodoList;
